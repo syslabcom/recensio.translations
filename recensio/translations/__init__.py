4049542310a936077d770704679c33263b8f5365
Manuel Reinhardt <reinhardt@syslab.com>
2020-04-28 09:46:17 +0200

    Black

--- recensio/translations/__init__.py
+++ recensio/translations/__init__.py
@@ -1,14 +1,15 @@
-  # -*- extra stuff goes here -*-
+# -*- extra stuff goes here -*-
 from zope.i18nmessageid import MessageFactory
 from zc.testbrowser.browser import Browser
 import pkg_resources
 import sys
 
-RecensioMessageFactory = MessageFactory('recensio')
+RecensioMessageFactory = MessageFactory("recensio")
 
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
 
 def podiff(vcsurl):
     """ Shows differences between the po files in the working copy of this
@@ -18,54 +19,57 @@ def podiff(vcsurl):
     import os, subprocess, tempfile, urllib, polib, re
     from mr.developer.common import which
 
-    vcstype = vcsurl.split(' ')[0]
-    vcsextra = ''
-    if len(vcsurl.split(' ')) > 2:
-        vcsextra = ' '.join(vcsurl.split(' ')[2:])
-    vcsurl = vcsurl.split(' ')[1]
+    vcstype = vcsurl.split(" ")[0]
+    vcsextra = ""
+    if len(vcsurl.split(" ")) > 2:
+        vcsextra = " ".join(vcsurl.split(" ")[2:])
+    vcsurl = vcsurl.split(" ")[1]
     pofiles = []
+
     def visit(pofiles, dirname, names):
         pofiles.extend(
-            map(lambda n: os.path.join(dirname, n),
-                filter(
-                    lambda n: n.endswith('.po') or n.endswith('.pot'), names)))
+            map(
+                lambda n: os.path.join(dirname, n),
+                filter(lambda n: n.endswith(".po") or n.endswith(".pot"), names),
+            )
+        )
+
     os.path.walk(__path__[0], visit, pofiles)
-    basepath = __path__[0][:__path__[0].rfind(__name__)+len(__name__)]
+    basepath = __path__[0][: __path__[0].rfind(__name__) + len(__name__)]
 
     for pofile in pofiles:
-        #out = subprocess.Popen(("/usr/bin/svn info %s" % pofile).split(" "), stdout=subprocess.PIPE).stdout.read()
-        #out = out[out.find('URL: ')+5:]
-        #pofileurl = out[:out.find('\n')]
+        # out = subprocess.Popen(("/usr/bin/svn info %s" % pofile).split(" "), stdout=subprocess.PIPE).stdout.read()
+        # out = out[out.find('URL: ')+5:]
+        # pofileurl = out[:out.find('\n')]
         proto, string = urllib.splittype(vcsurl)
         host, path = urllib.splithost(string)
         relpath = os.path.relpath(pofile, basepath)
         pofileurl = os.path.join(vcsurl, relpath)
 
         tmp, tmppath = tempfile.mkstemp(text=True)
-        if vcstype == 'svn':
+        if vcstype == "svn":
             urllib.urlretrieve(pofileurl, tmppath)
-        elif vcstype == 'git':
-            cmd = which('git')
-            branchmatch = re.search('branch=(\S*)', vcsextra)
+        elif vcstype == "git":
+            cmd = which("git")
+            branchmatch = re.search("branch=(\S*)", vcsextra)
             if branchmatch:
                 branch = branchmatch.group(1)
             else:
-                branch = 'master'
-            cmdline = '%s show %s:%s' % (cmd, branch, relpath)
+                branch = "master"
+            cmdline = "%s show %s:%s" % (cmd, branch, relpath)
             err, errtmppath = tempfile.mkstemp(text=True)
-            out = subprocess.Popen(cmdline.split(' '), 
-                  stdout=subprocess.PIPE,
-                  stderr=err,
-                  cwd=basepath).stdout.read()
+            out = subprocess.Popen(
+                cmdline.split(" "), stdout=subprocess.PIPE, stderr=err, cwd=basepath
+            ).stdout.read()
             err = open(errtmppath).read()
             if err:
-                print(err)
+                print (err)
                 return
-            outfile = open(tmppath, 'w')
+            outfile = open(tmppath, "w")
             outfile.write(out)
             outfile.close()
         else:
-            print('Sorry, %s is not supported yet.')
+            print ("Sorry, %s is not supported yet.")
             return
 
         polocal = polib.pofile(pofile)
@@ -78,7 +82,7 @@ def podiff(vcsurl):
                 if entryvcs not in polocal.obsolete_entries():
                     diff += ['-#msgid "%s"' % entryvcs.msgid]
                     diff += ['-#msgstr "%s"' % entryvcs.msgstr]
-                    diff += ['']
+                    diff += [""]
                 else:
                     continue
             entrylocal = polocal.find(entryvcs.msgid)
@@ -86,17 +90,17 @@ def podiff(vcsurl):
                 if entryvcs in polocal.obsolete_entries():
                     diff += ['+#msgid "%s"' % entryvcs.msgid]
                     diff += ['+#msgstr "%s"' % entryvcs.msgstr]
-                    diff += ['']
+                    diff += [""]
                 else:
                     diff += ['-msgid "%s"' % entryvcs.msgid]
                     diff += ['-msgstr "%s"' % entryvcs.msgstr]
-                    diff += ['']
+                    diff += [""]
             else:
                 if not entryvcs.msgstr == entrylocal.msgstr:
                     diff += [' msgid "%s"' % entryvcs.msgid]
                     diff += ['-msgstr "%s"' % entryvcs.msgstr]
                     diff += ['+msgstr "%s"' % entrylocal.msgstr]
-                    diff += ['']
+                    diff += [""]
         for entrylocal in filter(lambda e: e not in povcs, polocal):
             if entrylocal in povcs.obsolete_entries():
                 # XXX WTF? Iterating over a POFile yields obsolete entries, but
@@ -115,39 +119,57 @@ def podiff(vcsurl):
             if entrylocal.obsolete:
                 diff += ['+#msgid "%s"' % entrylocal.msgid]
                 diff += ['+#msgstr "%s"' % entrylocal.msgstr]
-                diff += ['']
+                diff += [""]
             else:
                 diff += ['+msgid "%s"' % entrylocal.msgid]
                 diff += ['+msgstr "%s"' % entrylocal.msgstr]
-                diff += ['']
+                diff += [""]
 
         if diff:
-            out = ['']
-            out += ['Index: %s' % pofile]
-            out += ['===================================================================']
-            out += ['--- repository']
-            out += ['+++ working copy']
+            out = [""]
+            out += ["Index: %s" % pofile]
+            out += [
+                "==================================================================="
+            ]
+            out += ["--- repository"]
+            out += ["+++ working copy"]
             out += diff
-            out += ['']
-            print("\n".join(out))
+            out += [""]
+            print ("\n".join(out))
 
         os.remove(tmppath)
 
+
 def updateTranslations(ignore):
-    br = Browser('http://transifex.syslab.com')
+    br = Browser("http://transifex.syslab.com")
     try:
-        br.getControl(name='username').value = sys.argv[1]
+        br.getControl(name="username").value = sys.argv[1]
         if len(sys.argv) < 3:
             import getpass
-            br.getControl(name='password').value = getpass.getpass()
+
+            br.getControl(name="password").value = getpass.getpass()
         else:
-            br.getControl(name='password').value = sys.argv[2]
+            br.getControl(name="password").value = sys.argv[2]
     except IndexError:
         print "Usage: %s [Your Transifex username] [Your Transifex password]"
         return 1
-    br.getControl('Sign in').click()
-    for domain, url_tmpl in (('recensio', 'http://transifex.syslab.com/projects/p/recensio/resource/recensiopot/l/%s/download/'), ('plone', 'http://transifex.syslab.com/projects/p/recensio/resource/plonepot/l/%s/download/')):
-      for lang in ('de', 'en', 'fr'):
-        print "Getting %s for %s" % (domain, lang)
-        br.open(url_tmpl % lang)
-        file(pkg_resources.resource_filename(__name__, 'locales/%s/LC_MESSAGES/%s.po' % (lang, domain)), 'w').write(br.contents)
+    br.getControl("Sign in").click()
+    for domain, url_tmpl in (
+        (
+            "recensio",
+            "http://transifex.syslab.com/projects/p/recensio/resource/recensiopot/l/%s/download/",
+        ),
+        (
+            "plone",
+            "http://transifex.syslab.com/projects/p/recensio/resource/plonepot/l/%s/download/",
+        ),
+    ):
+        for lang in ("de", "en", "fr"):
+            print "Getting %s for %s" % (domain, lang)
+            br.open(url_tmpl % lang)
+            file(
+                pkg_resources.resource_filename(
+                    __name__, "locales/%s/LC_MESSAGES/%s.po" % (lang, domain)
+                ),
+                "w",
+            ).write(br.contents)
--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
 from setuptools import setup, find_packages
 import os
 
-version = '3.3.1.dev0'
+version = "3.3.1.dev0"
 
-setup(name='recensio.translations',
-      version=version,
-      description="Translations for Recensio",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python :: 2.7",
-      ],
-      keywords='web zope plone translations',
-      author='SYSLAB.COM',
-      author_email='info@syslab.com',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['recensio'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'infrae.i18nextract',
-          'zope.i18nmessageid',
-          'zc.testbrowser'
-      ],
-      extras_require={
-          'podiff': ['polib', 'mr.developer'],
-      },
-      entry_points="""
+setup(
+    name="recensio.translations",
+    version=version,
+    description="Translations for Recensio",
+    long_description=open("README.txt").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=["Framework :: Plone", "Programming Language :: Python :: 2.7",],
+    keywords="web zope plone translations",
+    author="SYSLAB.COM",
+    author_email="info@syslab.com",
+    url="http://svn.plone.org/svn/collective/",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["recensio"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "infrae.i18nextract",
+        "zope.i18nmessageid",
+        "zc.testbrowser",
+    ],
+    extras_require={"podiff": ["polib", "mr.developer"],},
+    entry_points="""
       # -*- Entry points: -*-
 
       [z3c.autoinclude.plugin]
@@ -41,4 +38,4 @@ setup(name='recensio.translations',
       podiff = recensio.translations:podiff
       updateTranslations = recensio.translations:updateTranslations
       """,
-      )
+)
